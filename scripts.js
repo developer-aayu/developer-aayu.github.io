acbd55d0b781c5455588cf15a020c5c5ef76d13d
developer-aayu <aayusharma00123@gmail.com>
2024-07-14 07:48:18 +0530

    repo cloned

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var contactButton = document.getElementById("contactButton");
-    contactButton.addEventListener("click", function() {
-        alert("Thank you for reaching out!");
-    });
-});
